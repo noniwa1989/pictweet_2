7680e0a82fd685a1cadbf4ed6c17e2e917f5a05a
noniwa1989 <nr19891122@yahoo.co.jp>
2020-05-05 14:32:21 +0900

    マイページ表示可能

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -52,3 +52,17 @@ before_fork do |server, worker|
   
   after_fork do |_server, _worker|
     defined?(ActiveRecord::Ba
+
+
+      
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
+
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
